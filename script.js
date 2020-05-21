2ba341a5a09322c7539add94ac1dd316aa4955c3
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-05-21 14:39:54 +0100

    Inital release

---
+++ script.js
@@ -0,0 +1,6 @@
+// const burger = document.querySelector(".burger");
+// const navItems = document.querySelector(".nav-items");
+// burger.addEventListener("click", () => {
+//   burger.classList.toggle("open");
+//   navItems.classList.toggle("close");
+// });
